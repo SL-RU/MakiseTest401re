e8bc35cda65ecfb1936c37fd4cbce2db72144fc7
Alexander Lutsai <s.lyra@ya.ru>
2017-01-07 01:42:25 +0300

    GUI first steps

---
+++ Inc/Backup/makise_test.h.bak
@@ -0,0 +1,8 @@
+#include "makise.h"
+#include "makise_gui.h"
+#include "spi.h"
+#include "gpio.h"
+#include <string.h>
+
+MakiseGUI* mt_start();
+extern MHost *host;
---
+++ project.el
@@ -0,0 +1,21 @@
+(ede-cpp-root-project "MakiseTest401re"
+                      :name "MakiseTest401re"
+		      :spp-table '(("__weak" . "__attribute__((weak))")
+				    ("__packed" . "__attribute__((__packed__))")
+				    ("USE_HAL_DRIVER" . "")
+				    ("STM32F401xE" . ""))
+      		      :compile-command "make"
+                      :file "/mnt/doc/stm32/ili9340/MakiseTest401re/project.el"
+                      :include-path '("/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+                                      "/Drivers/CMSIS/Include"
+                                      "/Drivers/STM32F4xx_HAL_Driver/Inc"
+                                      "/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+                                      "/Inc"
+                                      "/MakiseGUI/MakiseGUI"
+                                      "/MakiseGUI/MakiseGUI/fonts"
+                                      "/MakiseGUI/MakiseGUI/gui"
+                                      "/MakiseGUI/MakiseGUI/ILI9340C"
+                                      "/Middlewares/Third_Party/FatFs/src"
+                                      "/Middlewares/Third_Party/FatFs/src/drivers"
+                                      )
+		      )
